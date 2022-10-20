9e7f90c741044d98d7819825d0156454b9e63859
KardaElena <karda.elena@gmail.com>
2022-10-20 15:17:58 +0300

    A task about coordinates added

---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Clear();
+
+Console.WriteLine("Введите номер четверти плоскости, в которой хотите задать координаты: ");
+
+int quarter = int.Parse(Console.ReadLine());
+
+while(quarter < 1 || quarter > 4)
+
+{
+Console.WriteLine("\nЭто не номер четверти. Введите цифру от 1 до 4: ");
+quarter = int.Parse(Console.ReadLine() ?? "0");
+}
+
+if (quarter == 1)
+    {
+        Console.WriteLine("\nВы можете использовать положительные цифры от 0 до бесконечности по X и Y");
+        Console.WriteLine();
+    }
+    else if (quarter == 2)
+    {
+        Console.WriteLine("\nВы можете использовать положительные цифры от 0 до бесконечности по Y и отрицательные от 0 до бесконечности по X");
+        Console.WriteLine();
+    }
+    else if (quarter == 3)
+    {
+        Console.WriteLine("\nВы можете использовать отрицательные цифры от 0 до бесконечности по X и Y");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine("\nВы можете использовать отрицательные цифры от 0 до бесконечности по Y и положительные от 0 до бесконечности по X");
+        Console.WriteLine();
+    }
